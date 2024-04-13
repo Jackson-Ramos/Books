9f6c113e1506627fcc6b8d503cb742fd7844f54e
Jackson Ramos <jackson.ramos@maisunifacisa.com.br>
2024-04-12 21:58:51 -0300

    Adição do controller

---
+++ src/main/java/br/org/unifacisa/booksapi/controller/BooksController.java
@@ -0,0 +1,9 @@
+package br.org.unifacisa.booksapi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/books")
+public class BooksController {
+}
