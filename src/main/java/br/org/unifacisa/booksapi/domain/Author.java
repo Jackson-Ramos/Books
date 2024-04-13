ef3ec7ae11abc31225d02564d1c0743246bfc267
Jackson Ramos <jackson.ramos@maisunifacisa.com.br>
2024-04-13 20:56:46 -0300

    Atualização

--- src/main/java/br/org/unifacisa/booksapi/domain/Author.java
+++ src/main/java/br/org/unifacisa/booksapi/domain/Author.java
@@ -7,23 +7,18 @@ import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
-@Data
 @Entity
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Book {
+@Table(name = "author")
+public class Author {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.UUID)
 	@Column(unique = true, nullable = false)
 	private String id;
 	
-	@Column(name = "title", nullable = false)
-	private String title;
-	
-	@Column(name = "author", nullable = false)
-	private String author;
-	
-	@Column(name = "description", nullable = false)
-	private String description;
+	@Column(name = "name", nullable = false)
+	private String name;
 }
