b27e5a2b3b913ea6aca55d98ae85ecbd53acdf46
Jackson Ramos <jackson.ramos@maisunifacisa.com.br>
2024-04-12 21:41:35 -0300

    first commit

---
+++ src/test/java/br/org/unifacisa/booksapi/BooksApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.org.unifacisa.booksapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksApiApplicationTests {
+	
+	@Test
+	void contextLoads() {
+	}
+	
+}
