293cb0ad71e58f3f650a5d959b88dc3fdd714ef9
Jackson Ramos <jackson.ramos@maisunifacisa.com.br>
2024-04-30 20:37:07 -0300

    Adição de novas dependencias e uma nova entidade User

--- src/main/java/br/org/unifacisa/booksapi/domain/user/User.java
+++ src/main/java/br/org/unifacisa/booksapi/domain/user/User.java
@@ -1,4 +1,41 @@
 package br.org.unifacisa.booksapi.domain.user;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "users")
+@Table(name = "users")
 public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
+	@Column(unique = true, nullable = false)
+	private String id;
+	
+	@Column(name = "login",unique = true, nullable = false)
+	private String login;
+	
+	@Column(name = "full_name", nullable = false)
+	private String fullName;
+	
+	@Column(name = "password", nullable = false)
+	private String password;
+	
+	@Column(name = "account_non_expired")
+	private String accountNonExpired;
+	
+	@Column(name = "account_non_locked")
+	private String accountNonLocked;
+	
+	@Column(name = "credentials_non_expired")
+	private String credentialsNonExpired;
+	
+	@Column(name = "enabled")
+	private String enabled;
+	
 }
